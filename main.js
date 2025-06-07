f3701f9f58ac091746ff188e92251a11b1d3b29d
lam170392 <lam170392@gmail.com>
2025-06-07 20:37:59 +0300

    Adding toggle switch theme

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-'use strict'
-
-const btnToggle = document.getElementById('themes_toggle');
-
-btnToggle.addEventListener('click', () => {
-    const isDark = document.body.classList.toggle('dark');
-    btnToggle.textContent = isDark ? 'Light' : 'Dark';
-});
+console.log('Happy developing ✨')
