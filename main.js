5be0cde4c382d8498efa2a11527bfc45e1498d78
lam170392 <lam170392@gmail.com>
2025-06-07 20:36:52 +0300

    first template

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+'use strict'
+
+const btnToggle = document.getElementById('themes_toggle');
+
+btnToggle.addEventListener('click', () => {
+    const isDark = document.body.classList.toggle('dark');
+    btnToggle.textContent = isDark ? 'Light' : 'Dark';
+});
